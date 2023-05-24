5e3628c5302b4135e54281a786a149cb06ed875b
ompus <92881768+ompus@users.noreply.github.com>
2023-05-24 14:18:31 +0000

    add test_correct.rb

--- lib/sabetsuka/polynomial.rb
+++ lib/sabetsuka/polynomial.rb
@@ -1,52 +1,55 @@
 module Sabetsuka
-class Polynomial
-  attr_accessor :expression
+  class Polynomial
+    attr_accessor :expression
 
-  def initialize(expression)
-    @expression = expression
-  end
+    def initialize(expression)
+      @expression = expression
+    end
 
-  def differentiate
-    terms = @expression.split('+')
-    derivative_terms = []
+    def differentiate
+      terms = @expression.split('+')
+      derivative_terms = []
 
-    terms.each do |term|
-      coefficient, variable, exponent = parse_term(term)
+      terms.each do |term|
+        coefficient, variable, exponent = parse_term(term)
 
-      puts variable
+        puts variable
 
-      if exponent > 0 && variable != "none"
+        next unless exponent > 0 && variable != 'none'
+
         derivative_coefficient = coefficient * exponent
         derivative_exponent = exponent - 1
         derivative_terms << format_term(derivative_coefficient, derivative_exponent)
       end
+
+      derivative_expression = derivative_terms.join(' + ')
+      Polynomial.new(derivative_expression)
     end
 
-    derivative_expression = derivative_terms.join(' + ')
-    Polynomial.new(derivative_expression)
+    def to_s
+      @expression
+    end
+
+    private
+
+    def parse_term(term)
+      term.strip!
+      matches = term.match(/([\d\-]*)([a-zA-Z]*)(\^(\d+))?/)
+
+      coefficient = matches[1].nil? ? 1 : matches[1].to_i
+      variable = matches[2].empty? ? 'none' : matches[2].to_s
+      exponent = matches[4].nil? ? 1 : matches[4].to_i
+
+      [coefficient, variable, exponent]
+    end
+
+    def format_term(coefficient, exponent)
+      term = coefficient.abs.to_s
+      term += 'x' if exponent > 0
+      term += "^#{exponent}" if exponent > 1
+      # term.prepend('-') if coefficient.negative?
+
+      term
+    end
   end
-
-  private
-
-  def parse_term(term)
-    term.strip!
-    matches = term.match(/([\d\-]*)([a-zA-Z]*)(\^(\d+))?/)
-
-    coefficient = matches[1].nil? ? 1 : matches[1].to_i
-    variable = matches[2].empty? ? "none" : matches[2].to_s
-    exponent = matches[4].nil? ? 1 : matches[4].to_i
-
-    [coefficient, variable, exponent]
-  end
-
-  def format_term(coefficient, exponent)
-    term = coefficient.abs.to_s
-    term += 'x' if exponent > 0
-    term += "^#{exponent}" if exponent > 1
-    # term.prepend('-') if coefficient.negative?
-
-    term
-  end
-end
-
 end
--- test/test_correct.rb
+++ test/test_correct.rb
@@ -1,8 +1,10 @@
 require 'test/unit'
+require_relative '../lib/sabetsuka'
+
 class MyMethodTest < Test::Unit::TestCase
 
   def test_zeroes
-    assert_equal(0, 1)
+    assert_equal("9x^2 + 8x + 5", Sabetsuka::Polynomial.new("3x^3 + 4x^2 + 5x + 4").differentiate.to_s)
   end
 
 end
