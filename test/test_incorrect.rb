ee628a3e761c4467899566e3d3a696838a36c0ca
Gladosmark <92906674+Gladosmark@users.noreply.github.com>
2023-06-03 09:39:59 +0000

    Update test_incorrect

--- test/test_incorrect.rb
+++ test/test_incorrect.rb
@@ -8,32 +8,43 @@ class MyMethodTest < Test::Unit::TestCase
     end
   end
 
-  #def test_two_signs
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("2x^3 ++ 9x", "x").differentiate.to_s)
-  # end
+  def test_two_signs
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+      Sabetsuka::Polynomial.new("2x^3 ++ 9x", "x").differentiate.to_s
+   end
+end
 
-  #def test_sign_without_term
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("2x^3 + + 5", "y").differentiate.to_s)
-  #end
+  def test_sign_without_term
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+    Sabetsuka::Polynomial.new("2x^3 + + 5", "y").differentiate.to_s
+  end
+end
 
-  #def test_sign_without_first_term
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("+ 2x^3", "x").differentiate.to_s)
-  #end
+ 
 
-  #def test_sign_without_second_term
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("2x^3 + 9x^2 +" , "x").differentiate.to_s)
-  #end
+  def test_sign_without_second_term
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+    Sabetsuka::Polynomial.new("2x^3 + 9x^2 +" , "x").differentiate.to_s
+  end
+end
 
-  #def test_letter_deg
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("2x^a + 5", "y").differentiate.to_s)
-  #end
+  def test_letter_deg
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+    Sabetsuka::Polynomial.new("2x^a + 5", "y").differentiate.to_s
+  end
+end
 
-  #def test_deg_without_var
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("^4 + 56", "x").differentiate.to_s)
-  #end
+  def test_deg_without_var
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+    Sabetsuka::Polynomial.new("^4 + 56", "x").differentiate.to_s
+  end
+end
 
-  #def test_deg_without_num
-  #assert_equal(ArgumentError, Sabetsuka::Polynomial.new("x^ + 56", "x").differentiate.to_s)
-  #end
+  def test_deg_without_num
+  assert_raises Sabetsuka::InvalidSyntaxError do 
+    Sabetsuka::Polynomial.new("x^ + 56", "x").differentiate.to_s
+  end
+end
+
 end
 
